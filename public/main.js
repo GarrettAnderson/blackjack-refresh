701e766ea161b34e0bb10215653b0bbaa599ce4f
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-09 17:38:59 -0400

    added images for each of the cards and the card back.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,19 @@
 const suits = ['hearts', 'diamonds', 'clubs', 'spades']
-const ranks = [{name: 'ace', value: 11}, '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'king', 'queen']
+const ranks = [
+  {name: 'ace', value: 11}, 
+  {name: '2', value: 2}, 
+  {name: '3', value: 3},
+  {name: '4', value: 4},
+  {name: '5', value: 5},
+  {name: '6', value: 6},
+  {name: '7', value: 7},
+  {name: '8', value: 8},
+  {name: '9', value: 9},
+  {name: '10', value: 10},
+  {name: 'jack', value: 10},
+  {name: 'queen', value: 10},
+  {name: 'king', value: 10},
+]
 let deck = []
 
 // let deckOfCardsURL = 'https://www.deckofcardsapi.com/api/deck/new/'
@@ -13,27 +27,40 @@ let deck = []
 // console.log(deckOfCards)
 
 
+const createDeck = () => {
+  console.log('create deck of cards')
 
-const main = () => {
-  console.log('shuffle the cards')
+  // for(let i = 0; i < suits.length; i++) {
+  //   for(let j = 0; j < ranks.length; j++) {
 
-  for(let i = 0; i < suits.length; i++) {
-    for(let j = 0; j < ranks.length; j++) {
+  //     const card = {
+  //       suit: suits[i],
+  //       rank: ranks[j]
+  //     }
+  //     console.log(card)
+  //     deck.push(card)
 
-      const card = {
-        suit: suits[i],
-        rank: ranks[j]
-      }
-      console.log(card)
-      deck.push(card)
+  //   }
+  // }
+  // console.log(deck)
 
-    }
+    suits.forEach(suit => {
+      ranks.forEach(rank => {
+        const card = {
+          value: rank.value,
+          suit: suit,
+          rank: rank.name,
+          imageUrl: `${suit}_${rank.name}.png`
+        }
+        deck.push(card)
+      })
+    })
+    console.log(deck)
   }
-  console.log(deck)
 
 // Fisher-Yates shuffle
 
-
+const shuffleDeck = () => {
   for (let i = deck.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     console.log(j) 
@@ -46,6 +73,8 @@ const main = () => {
   }
   console.log(deck)
 }
+
+
 
 const dealCard = () => {
   //remove the 1st card from our deck
@@ -62,6 +91,11 @@ const dealCard = () => {
 
   // update the UI
   document.querySelector('.playerHand').appendChild(cardElement)
+}
+
+const main = () => {
+  createDeck()
+  shuffleDeck()
 }
 
 
